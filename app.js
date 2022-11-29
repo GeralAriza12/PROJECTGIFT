0be4e9f8c8b3d20c52204caf92ebb15b3a5be346
123Bfeo <ldnr13@hotmail.com>
2022-11-28 20:13:41 -0500

    estructura de proyecto

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express();
+
+
+
+
+
+const port = 3015;
+app.listen(port, (req, res) => {
+    console.log("SERVER RUNNING 😊");
+})
+
+
+
+
---
+++ public/js/fun.js
@@ -0,0 +1,5 @@
+function sumar(num1, num2) {
+    var sumar = num1 + num2;
+    console.log(sumar)
+}
+sumar(5, 8);
